b000d0f7acde9b8ede71dd11cb575a00ce1de089
Prasant Singh <121022092+Prasant-singh@users.noreply.github.com>
2025-08-26 16:08:19 +0530

    Update main.py

--- Transformer/main.py
+++ Transformer/main.py
@@ -2,23 +2,27 @@
 
 from transformers import AutoTokenizer
 
+# Getting the exact same tokenizer that model was trained with
 checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
+# Converting input to tokens to pass to the model
 input=["I am felling happy today","I will break your teeth into peices"]
 inputs=tokenizer(input,return_tensors="pt",padding=True,truncation=True)
-print(inputs)
+print("Inputs:", inputs)
 
 
-
+# Importing the model And using Auto model class so that it can automatically finds the model that best fit with situation
 from transformers import AutoModelForSequenceClassification
 model=AutoModelForSequenceClassification.from_pretrained(checkpoint)
 
+# Passing the inputs to the model FORWARD PASS
 outputs=model(**inputs)
+print("Size of the image pass to the HEAD layer:", outputs.last_hidden_state.shape)
+# Cheching shape of logits. In short cheching the shape of logits that HEAD passes to the output layer
 print(outputs.logits.shape)
-
 import torch
 prediction=torch.nn.functional.softmax(outputs.logits,dim=-1)
-print(prediction)
+print("Prediction:", prediction)
 
-print(model.config.id2label)
+print("Label Mapping:", model.config.id2label)
